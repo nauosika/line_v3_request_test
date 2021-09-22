f9c8cfb5f8147c93f6ab77a2436823031f9d1fe0
nauosika <nauosikaforwork@gmail.com>
2021-09-22 09:17:39 +0800

    select_tag

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(
+  name: "測試商品",
+  price: 500
+)
